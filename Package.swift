d585bec7a6febc6131d114360bec43031dfe30d8
pratikchovatiya_ebiz <pratikchovatiya@esparkbizmail.com>
2021-11-26 18:54:39 +0530

    intial Commit

--- Package.swift
+++ Package.swift
@@ -12,8 +12,9 @@ let package = Package(
             targets: ["MyNewPackage"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Firebase",
+                   url: "https://github.com/firebase/firebase-ios-sdk.git",
+                   from: "8.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/MyNewPackage/MyNewPackage.swift
+++ Sources/MyNewPackage/MyNewPackage.swift
@@ -2,5 +2,10 @@ public struct MyNewPackage {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        getData()
+    }
+    
+    public func getData() {
+        print("hello")
     }
 }
